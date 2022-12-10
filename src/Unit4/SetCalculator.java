83dfa9f035ccbe7834449018373a31327e656d71
Ruhan <ruhan.swanepoel17@gmail.com>
2022-12-10 11:31:06 +0200

    Clean

--- src/Unit4/SetCalculator.java
+++ src/Unit4/SetCalculator.java
@@ -13,26 +13,26 @@ public class SetCalculator {
         right = new TreeSet<>();
     }
 
-    public static void main(String[] args) {
-        while (true) {
-            System.out.println("\n\nEnter a fully parenthesized expression,");
-            System.out.println("or press return to end.");
-            System.out.print("\n?  ");
-            TextIO.skipBlanks();
-            if (TextIO.peek() == '\n')
-                break;
-            try {
-                TreeSet<Integer> result = expressionValue();
-                TextIO.skipBlanks();
-                if (TextIO.peek() != '\n')
-                    throw new ParseError("Extra data after end of expression.");
-                TextIO.getln();
-                System.out.println("\nValue is " + val);
-            } catch (ParseError e) {
-                System.out.println("\n*** Error in input:    " + e.getMessage());
-                System.out.println("*** Discarding input:  " + TextIO.getln());
-            }
-        }
+//    public static void main(String[] args) {
+//        while (true) {
+//            System.out.println("\n\nEnter a fully parenthesized expression,");
+//            System.out.println("or press return to end.");
+//            System.out.print("\n?  ");
+//            TextIO.skipBlanks();
+//            if (TextIO.peek() == '\n')
+//                break;
+//            try {
+////                TreeSet<Integer> result = expressionValue();
+//                TextIO.skipBlanks();
+//                if (TextIO.peek() != '\n')
+////                    throw new ParseError("Extra data after end of expression.");
+//                TextIO.getln();
+//                System.out.println("\nValue is " + val);
+////            } catch (ParseError e) {
+////                System.out.println("\n*** Error in input:    " + e.getMessage());
+//                System.out.println("*** Discarding input:  " + TextIO.getln());
+//            }
+//        }
     }
 
 }
