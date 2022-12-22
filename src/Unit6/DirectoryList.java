ff9d162c08adb5fb3f2eb813276039e959351949
Ruhan <ruhan@ibureau.services>
2022-12-22 20:54:20 +0200

    Unit 6 Journal

--- src/Unit6/DirectoryList.java
+++ src/Unit6/DirectoryList.java
@@ -1,6 +1,7 @@
 package Unit6;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -12,32 +13,49 @@ import java.util.Scanner;
 public class DirectoryList {
 
 
-   public static void main(String[] args) {
+    public static void main(String[] args) {
 
-      String directoryName;  // Directory name entered by the user.
-      File directory;        // File object referring to the directory.
-      String[] files;        // Array of file names in the directory.
-      Scanner scanner;       // For reading a line of input from the user.
+        String directoryName;  // Directory name entered by the user.
+        File directory;        // File object referring to the directory.
+        String[] files;        // Array of file names in the directory.
+        Scanner scanner;       // For reading a line of input from the user.
+        ArrayList<String[]> directoryTree = new ArrayList<>();
 
-      scanner = new Scanner(System.in);  // scanner reads from standard input.
+        scanner = new Scanner(System.in);  // scanner reads from standard input.
 
-      System.out.print("Enter a directory name: ");
-      directoryName = scanner.nextLine().trim();
-      directory = new File(directoryName);
+        System.out.print("Enter a directory name: ");
+        directoryName = scanner.nextLine().trim();
+        directory = new File(directoryName);
 
-      if (directory.isDirectory() == false) {
-         if (directory.exists() == false)
-            System.out.println("There is no such directory!");
-         else
-            System.out.println("That file is not a directory.");
-      }
-      else {
-         files = directory.list();
-         System.out.println("Files in directory \"" + directory + "\":");
-         for (int i = 0; i < files.length; i++)
-            System.out.println("   " + files[i]);
-      }
+        if (!directory.isDirectory()) {
+            if (!directory.exists())
+                System.out.println("There is no such directory!");
+            else
+                System.out.println("That file is not a directory.");
+        } else {
+            listDirectory(directory);
+        }
 
-   } // end main()
+    } // end main()
+
+    private static void listDirectory(File directory) {
+        System.out.println("\t" + directory);
+        System.out.print("\t");
+        String[] files;
+        files = directory.list();
+
+        for (int i = 0; i < files.length; i++) {
+            File f;
+            f = new File(directory, files[i]);
+
+            if (f.isDirectory()) {
+                listDirectory(f);
+
+            } else {
+                System.out.print("\t\t " + files[i] + "\n");
+            }
+
+        }
+    }
 
 } // end class DirectoryList
